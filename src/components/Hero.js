8c6b42bdca82e1b4c6c2bbd0a5ba5299d821ab28
dreat <lukasz@hawku.com>
2024-10-07 19:19:48 +0200

    try to update the webpage

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,18 +8,17 @@ function Hero() {
         <div className='grid md:grid-cols-2 py-20 sm:px-5 md:px-20 items-center'>
             <div className='md:pl-10'>
                 <h1 className='font-bold text-6xl md:text-7xl text-red-600'>Music Jam</h1>
-                <p>Bajt na bicie</p>
                 <div className='py-10 md:py-16'>
-                    <p>4.11.2023</p>
+                    <p>7-8.11.2024</p>
                     <p>Instytut Kultury Miejskiej</p>
                     <p>Gdańsk</p>
                 </div>
-                <a href="https://app.evenea.pl/event/musicjam2023/" target='_blank'>
+                <a href="https://app.evenea.pl/event/musicjam2024/" target='_blank'>
                 <button className='text-white font-semibold mt-2 sm:mt-0 font-normal bg-red-600 rounded-md px-4 py-1 md:py-2 hover:scale-105 duration-500 text-xl md:text-2xl lg:text-4xl'>Weź udział</button>
                 </a>
             </div> 
             <div className='w-3/4 md:w-full mx-auto'>
-                <img src={hero} alt="hero image"/>
+                <video autoplay="" controls="" loop="" muted="" src="https://hackathons.ikm.gda.pl/wp-content/uploads/2024/08/music-jam-cover-photo-fb.mp4" playsinline=""></video>
             </div>
         </div>
     </div>
