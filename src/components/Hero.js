5fec3c17c9f2803887b6429371abd7d9bb155df1
dreat <lukasz@hawku.com>
2024-10-07 19:39:25 +0200

    why tailwind was used

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,14 +13,14 @@ function Hero() {
                     <p>Instytut Kultury Miejskiej</p>
                     <p>Gdańsk</p>
                 </div>
-                <div className='py-10 md:py-16'>
-                <p>
+                <div>
+                <p className='py-2 md:py-4'>
                     <a href="https://app.evenea.pl/event/musicjam2024/" target='_blank'>
                         <button className='text-white font-semibold mt-2 sm:mt-0 font-normal bg-red-600 rounded-md px-4 py-1 md:py-2 hover:scale-105 duration-500 text-xl md:text-2xl lg:text-4xl'>Weź udział</button>
                     </a>
                 </p>
 
-                <p>
+                <p className='py-2 md:py-4'>
                     <a href="https://hackathons.ikm.gda.pl/" target='_blank'>
                         <button className='text-white font-semibold mt-2 sm:mt-0 font-normal bg-red-600 rounded-md px-4 py-1 md:py-2 hover:scale-105 duration-500 text-xl md:text-2xl lg:text-4xl'>Dowiedz się więcej</button>
                     </a>
